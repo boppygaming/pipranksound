e993f07803c2e3e016e72d3497662ee51f353e5b
Liam <boppygaming@gmail.com>
2016-02-06 21:28:15 +0000

    added menu to play sound

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Pranks<br><a href="/play">Horn</a>'
+
+@app.route('/play')
+def play():
+    os.system("omxplayer -o local /home/pi/pipranksound/sounds/horn.mp3")
+    return 'You played a prank sound!<br><a href="/">Back to Menu</a>'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
