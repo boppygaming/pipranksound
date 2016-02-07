6f5d1b87375f9ef8ab0af6828bb4d4ddc009e588
Liam <boppygaming@gmail.com>
2016-02-07 22:36:21 +0000

    added templates and playing sound in background

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Pranks<br><a href="/play">Horn</a>'
+    #return ' 1 Pranks<br><a href="/play">Horn</a>'
+    return render_template('index.html')
 
 @app.route('/play')
 def play():
-    os.system("omxplayer -o local /home/pi/pipranksound/sounds/horn.mp3")
-    return 'You played a prank sound!<br><a href="/">Back to Menu</a>'
+    os.system("mpg321 /home/pi/pipranksound/sounds/horn.mp3&")
+    return 'You played a prank sound!<br><a href="/">Back to Menu</a>'  
 
 
 if __name__ == '__main__':
